8bc1178c74e87926d3aa8a2cf9b44f45d0be7d56
Shahar Seifer <66005045+Pr4Et@users.noreply.github.com>
2021-01-27 19:25:50 +0200

    Post processing
    Matlab programs to handle MAT files

---
+++ Post_Processing/balance_pic.m
@@ -0,0 +1,27 @@
+function normpic2=balance_pic(normpic,Nshades)    
+%Written by Shahar Seifer, Weizmann Institute of Science, 2020
+%H=histogram(normpic,'BinMethod','auto');
+[BinValues,BinEdges]=histcounts(normpic);
+NumBins=length(BinValues);    
+sumH=sum(BinValues);
+    temp=0;
+    for n=1:NumBins-1
+        temp=temp+BinValues(n);
+        if temp>0.005*sumH
+            lowedge=BinEdges(n);
+        break;
+        end
+    end
+    temp=0;
+    for n2=NumBins:-1:2
+        temp=temp+BinValues(n2);
+        if temp>0.005*sumH
+            highedge=BinEdges(n2);
+        break;
+        end
+    end
+    normpic(normpic>highedge)=highedge; %remove white dots
+    normpic(normpic<lowedge)=lowedge; %remove black dots
+    normpic2=(normpic-lowedge)*Nshades/(highedge-lowedge);
+return 
+    
